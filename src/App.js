a0f030ee04a3cc8d3a114f66338164c1817d318c
Liam Wilbur <lwilbs@liams-mbp.gusd.lcl>
2023-02-07 09:38:42 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import ChimpTest from "./ChimpTest";
+import './ChimpTest.css'
 import './App.css';
+import { render } from "@testing-library/react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  let counter = 1;
+  function createButtons() {
+    var myButtons = [];
+    for (let i = 0; i < counter; i++) {
+      var but = <button
+                    style={"color:blue"} 
+                    onClick={i => createButtons()}> 
+                    i
+                </button>
+      const randomLeftValue = Number.parseInt(Math.random() * (1000 - 10) + 10);
+      const randomTopValue =  Number.parseInt(Math.random() * (1000 - 10) + 10);
+      but.style.top = `${randomLeftValue}px`;
+      but.style.left = `${randomTopValue}px`;
+      myButtons.push(but)
+      counter++;
+    }
+    console.log("yo")
+    render(); {
+      return (
+        <div>{myButtons }</div>
+      );
+    }
+    // const button = document.querySelector("button");
+    // const randomLeftValue = Number.parseInt(Math.random() * (1000 - 10) + 10);
+    // const randomTopValue =  Number.parseInt(Math.random() * (1000 - 10) + 10);
+    // button.style.top = `${randomLeftValue}px`;
+    // button.style.left = `${randomTopValue}px`;
+  }
+  function newButton(data) {
+    return (
+      <button 
+          onClick={() => createButtons(data.label)}> //pass parameter for callback here if binding isn't used
+          data.name
+      </button>
   );
+  }
+  return (
+    <>
+    <ChimpTest />
+    {/* <div>{myButtons }</div> */}
+    <button onClick= {createButtons}>Start</button>
+    </>
+  )
 }
 
 export default App;
---
+++ src/ChimpTest.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './ChimpTest.css';
+
+export default function ChimpTest() {
+  return (
+    <>
+    <header className="header">
+      <h1 className="header-title">Chimp Test</h1>
+      <nav className="header-nav">
+        <a href="#about">Monkey Information</a>
+        {/* <a href="#services">Services</a>
+        <a href="#contact">Contact</a> */}
+      </nav>
+    </header>
+    <div className="title">
+        Monkey
+    </div>
+    </>
+  )
+}
