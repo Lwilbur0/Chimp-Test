fad2fe9308d874149d0936f9f8344c85042f310d
Liam Wilbur <lwilbs@liams-mbp.attlocal.net>
2023-03-05 22:13:11 -0800

    completed

--- src/App.js
+++ src/App.js
@@ -1,50 +1,142 @@
-import ChimpTest from "./ChimpTest";
+import React, { useEffect, useState } from "react"; 
 import './ChimpTest.css'
 import './App.css';
-import { render } from "@testing-library/react";
 
 function App() {
-  let counter = 1;
+  const [counter, setCounter] = useState(0);
+  const [nextButtonNumber, setNextButtonNumber] = useState(1);
+  const [fail, setFail] = useState(false);
+  const [button2Visible, setButton2Visible] = useState(false);
+  const [clickedButtonIndex, setClickedButtonIndex] = useState(false);
+  const [click, setClick] = useState(0);
+  const [buttons, setButtons] = useState([]);
+  const [buttonColor, setButtonColor] = useState("white");
+
+  var myButtons = [];
+  function isValidPosition(num, usedValues) {
+    for (let i = 0; i < usedValues.length; i++) {
+      if (Math.abs(usedValues[i] - num) < 50) {
+        return false;
+      }
+    }
+    return true;
+  }
   function createButtons() {
-    var myButtons = [];
-    for (let i = 0; i < counter; i++) {
-      var but = <button
-                    style={"color:blue"} 
-                    onClick={i => createButtons()}> 
-                    i
-                </button>
-      const randomLeftValue = Number.parseInt(Math.random() * (1000 - 10) + 10);
-      const randomTopValue =  Number.parseInt(Math.random() * (1000 - 10) + 10);
-      but.style.top = `${randomLeftValue}px`;
-      but.style.left = `${randomTopValue}px`;
+    myButtons = [];
+    var usedX = [];
+    var usedY = [];
+    let randomLeftValue = 0;
+    let randomTopValue = 0;
+    if (Array.isArray(myButtons) && myButtons.length > 0) {
+    myButtons.splice(0, myButtons.length)
+    }
+
+
+    for (let i = 1; i < counter + 1; i++) {
+      while (true) {
+        randomLeftValue = Number.parseInt(Math.random() * (650 - 10) + 400);
+        if (isValidPosition(randomLeftValue, usedX)) {
+          usedX.push(randomLeftValue);
+          break;
+        }
+      }
+      while (true) {
+        randomTopValue =  Number.parseInt(Math.random() * (400 - 10) + 150);
+        if (isValidPosition(randomTopValue, usedY)) {
+          usedY.push(randomLeftValue);
+          break;
+        } }
+      const className = true === clickedButtonIndex ? 'button-19 clicked' : 'button-19';
+      var but = <div key ={i}>
+        {button2Visible && 
+                <button
+                    className ={className}
+                    role = "button"
+                    style={{top: `${randomTopValue}px`, left: `${randomLeftValue}px`, color: buttonColor, maxWidth: '0px'}} 
+                    onClick={() => setClick(i)}
+                    id = {i}
+                    key={i}> 
+                    {i}
+                </button> }
+                </div>
       myButtons.push(but)
-      counter++;
     }
-    console.log("yo")
-    render(); {
-      return (
-        <div>{myButtons }</div>
-      );
+    return myButtons
+  }
+  useEffect(() => {
+    setButtons(createButtons());
+  }, [counter]);
+  useEffect(() => {
+    console.log(nextButtonNumber);
+  }, [nextButtonNumber]);
+  useEffect(() => {
+    if (fail) {
+      setButtons([]);
     }
-    // const button = document.querySelector("button");
-    // const randomLeftValue = Number.parseInt(Math.random() * (1000 - 10) + 10);
-    // const randomTopValue =  Number.parseInt(Math.random() * (1000 - 10) + 10);
-    // button.style.top = `${randomLeftValue}px`;
-    // button.style.left = `${randomTopValue}px`;
+  }, [fail]);
+  useEffect(() => {
+    if (click != 0) {
+      buttonClick(click);
+    }
+  }, [click]);
+  
+  function buttonClick(buttonNumber) {
+    if (buttonNumber === counter && buttonNumber === nextButtonNumber) {
+      console.log("done");
+      setNextButtonNumber(1);
+      setCounter(counter + 1);
+      setClickedButtonIndex(false);
+      setButtons(createButtons());
+      setClick(0);
+    }
+    else if (buttonNumber === nextButtonNumber) {
+      const newButtons = buttons.map((button, index) => {
+        if (button.length != 0) {
+        return index === buttonNumber - 1 ? [] : React.cloneElement(button, {
+          children: React.cloneElement(button.props.children, {
+            style: {
+              ...button.props.children.props.style,
+              color: "#1CB0F6"
+            }
+          })
+        });
+      }
+        else {
+          return [];
+        }
+      });
+      setButtons(newButtons)
+      setNextButtonNumber(nextButtonNumber + 1);
+      setClickedButtonIndex(true);
+    }
+    else {
+      setButton2Visible(false);
+      setFail(true);
+    }
   }
-  function newButton(data) {
-    return (
-      <button 
-          onClick={() => createButtons(data.label)}> //pass parameter for callback here if binding isn't used
-          data.name
-      </button>
-  );
+  function clickStart() {
+    handleButtonClick();
+    setCounter(counter + 1);
   }
+  const [buttonVisible, setButtonVisible] = useState(true);
+  const handleButtonClick = () => {
+    setButtonVisible(false);
+    setButton2Visible(true);
+  };
   return (
     <>
-    <ChimpTest />
-    {/* <div>{myButtons }</div> */}
-    <button onClick= {createButtons}>Start</button>
+    <div>{buttons}</div>
+    <div>
+    {buttonVisible && <button onClick= {clickStart} className = "button-19" role = "button" id="button-start">Start</button> }
+    {fail ? (
+        <div className="fail-screen">
+          <h2>You failed!</h2>
+          <button className= "button-19" role = "button" id = "retry" onClick={() => window.location.reload()}>Retry</button>
+        </div>
+      ) : (
+        <div>{buttons}</div>
+      )}
+    </div>
     </>
   )
 }
--- src/ChimpTest.js
+++ src/ChimpTest.js
@@ -1,20 +1,69 @@
-import React from 'react'
+// import React from 'react'
+import React, { useEffect, useState } from "react"; 
 import './ChimpTest.css';
+import { createClient } from 'pexels';
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 
 export default function ChimpTest() {
+  const client = createClient('jAv5LK8pT5QHPSP6yeXZouIgmkDe6qsEThcuCFarJepUVb0iPikKEcM8');
+  const query = 'Monkey';
+  var image = '';
+  const numbers = Array.from({length: 20}, (_, i) => i + 1);
+  for (let i = numbers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+  }
+  const uniqueNums = numbers.slice(0, 20);
+
+  client.photos.search({ query, per_page: 30 }).then(photos => {
+    try {
+      console.log(photos.photos[0].src.original)
+      image = `
+        <img src="${photos.photos[uniqueNums[0]].src.original}" width="250" height="250" style= "position: absolute; top: 85px; left: 2%; border: 2px solid white;"/>
+        <img src="${photos.photos[uniqueNums[1]].src.original}" width="250" height="250" style="position: absolute; top: 365px; left: 2%; border: 2px solid white;"/>
+        <img src="${photos.photos[uniqueNums[2]].src.original}" width="250" height="250" style= "position: absolute; top: 85px; right: 2%; border: 2px solid white;"/>
+        <img src="${photos.photos[uniqueNums[3]].src.original}" width="250" height="250" style="position: absolute; top: 365px; right: 2%; border: 2px solid white;"/>
+      `;
+      document.querySelector('#images').innerHTML = image;
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  });
+  const [monkeyMode, setMonkeyMode] = useState(false);
+
+  const handleMonkeyModeClick = () => {
+    setMonkeyMode(!monkeyMode);
+  }
   return (
     <>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <header className="header">
       <h1 className="header-title">Chimp Test</h1>
       <nav className="header-nav">
-        <a href="#about">Monkey Information</a>
+        <a onClick={handleMonkeyModeClick}>{monkeyMode ? 'Monkey Mode: ON' : 'Monkey Mode: OFF'}</a>
         {/* <a href="#services">Services</a>
         <a href="#contact">Contact</a> */}
       </nav>
     </header>
-    <div className="title">
-        Monkey
+    <div className="board"></div>
+    {monkeyMode && (
+    <div id = "images">
+      {/* <img src = "https://images.pexels.com/photos/1207875/pexels-photo-1207875.jpeg" width = "200" height = "200"></img> */}
     </div>
+    )}
+    <div className="description">
+        <div className="video-block">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/Zz7ShiQqLQg?start=14" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+        </div>
+        <div className="description-block">
+          <div className ="card">
+          <h2>Chimpanzee Ability</h2>
+          <hr/>
+          <p>Chimpanzees have the innate ability to memorize patterns much faster than any human can. The following video displays the true dominance of chimpanzee mental capabilities. These chimps are capable of completing each round of the chimp test in seconds, failing only less than 10% of the time.</p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
